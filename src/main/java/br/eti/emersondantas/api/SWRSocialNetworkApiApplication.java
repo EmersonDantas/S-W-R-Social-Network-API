4bc1e40ceafac817b144a6cf07d284e2afc252c4
emersondantas <emerson.ruan@dce.ufpb.br>
2020-06-25 00:27:51 -0300

    first commit

---
+++ src/main/java/br/eti/emersondantas/api/SWRSocialNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package br.eti.emersondantas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SWRSocialNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SWRSocialNetworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/eti/emersondantas/api/SWRSocialNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.emersondantas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SWRSocialNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
