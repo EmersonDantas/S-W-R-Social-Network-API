ead513368392c8ee75cdfe7e38e91f7d16fc0a60
emersondantas <emerson.ruan@dce.ufpb.br>
2020-06-25 20:15:53 -0300

    Added Item domain class

---
+++ src/main/java/br/eti/emersondantas/api/rebel/Item.java
@@ -0,0 +1,50 @@
+package br.eti.emersondantas.api.rebel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+@ToString(exclude = {"rebel"})
+@Entity
+public class Item implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private int amount;
+
+    @NonNull
+    private int points;
+
+    @ManyToOne
+    @JoinColumn(name = "rebel_id")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Rebel rebel;
+
+}
