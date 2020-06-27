1a6d43584a79363ca35c0083b1a0e2c85ec44ce1
emersondantas <emerson.ruan@dce.ufpb.br>
2020-06-27 15:12:20 -0300

    Documenting & changed to test profile

--- src/main/java/br/eti/emersondantas/api/rebel/Negotiation.java
+++ src/main/java/br/eti/emersondantas/api/rebel/Negotiation.java
@@ -1,5 +1,6 @@
 package br.eti.emersondantas.api.rebel;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,9 @@ public class Negotiation implements Serializable {
 
     private static final long serialVersionUID = 5054504208217649675L;
 
+    @ApiModelProperty(notes = "Rebel negotiator items", name = "itemsFrom")
     private List<Item> itemsFrom = new ArrayList<>();
 
+    @ApiModelProperty(notes = "Items of the rebel who wants to negotiate", name = "itemsTo")
     private List<Item> itemsTo = new ArrayList<>();
 }
