2f7cb48e8e431835913e4be2302df5ad6e064fb6
emersondantas <emerson.ruan@dce.ufpb.br>
2020-06-27 02:39:49 -0300

    Removed unused imports

--- src/main/java/br/eti/emersondantas/api/rebel/Item.java
+++ src/main/java/br/eti/emersondantas/api/rebel/Item.java
@@ -9,10 +9,6 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -20,7 +16,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
 import java.io.Serializable;
 
 @Getter
