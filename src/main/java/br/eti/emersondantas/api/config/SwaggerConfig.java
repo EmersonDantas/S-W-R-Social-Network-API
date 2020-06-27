694bf2a82882dc8c509f962e82319cba7b71f92d
emersondantas <emerson.ruan@dce.ufpb.br>
2020-06-27 14:32:41 -0300

    Resolved bug in swagger

--- src/main/java/br/eti/emersondantas/api/config/SwaggerConfig.java
+++ src/main/java/br/eti/emersondantas/api/config/SwaggerConfig.java
@@ -2,21 +2,13 @@ package br.eti.emersondantas.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -29,16 +21,8 @@ public class SwaggerConfig {
                 .paths(PathSelectors.ant("/api/**"))
                 .build()
                 .apiInfo(metaData())
-                .useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.GET, responseMessageForGET());
-    }
+                .useDefaultResponseMessages(false);
 
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     private ApiInfo metaData() {
@@ -51,13 +35,4 @@ public class SwaggerConfig {
                 .build();
     }
 
-    private List<ResponseMessage> responseMessageForGET() {
-        return new ArrayList<ResponseMessage>() {{
-            add(new ResponseMessageBuilder()
-                    .code(500)
-                    .message("500 Internal server error!")
-                    .responseModel(new ModelRef("Error"))
-                    .build());
-        }};
-    }
 }
