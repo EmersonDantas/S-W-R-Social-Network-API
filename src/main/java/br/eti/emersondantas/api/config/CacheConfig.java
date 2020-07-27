4f854c7551a5330f1e98ce0a2f2a3cb0726689ab
emersondantas <emerson.ruan@dce.ufpb.br>
2020-07-26 23:58:02 -0300

    Added cache config to activate cache

---
+++ src/main/java/br/eti/emersondantas/api/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package br.eti.emersondantas.api.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
